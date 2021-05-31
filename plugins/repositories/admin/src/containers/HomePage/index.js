330b0b18025cc1417e0e43e9ecdb20e36ed28216
Eduardo Lima <dlima78@hotmail.com>
2021-05-31 20:16:54 -0300

    chore: add strapi-plugin-entity-relationship-chart

--- plugins/repositories/admin/src/containers/HomePage/index.js
+++ plugins/repositories/admin/src/containers/HomePage/index.js
@@ -1,20 +1,52 @@
-/*
- *
- * HomePage
- *
- */
+import React, { useState, useEffect, memo } from 'react'
+import { Header } from '@buffetjs/custom'
+import { Table } from '@buffetjs/core'
+import axios from 'axios'
+import styled from 'styled-components'
 
-import React, { memo } from 'react';
-// import PropTypes from 'prop-types';
-import pluginId from '../../pluginId';
+const Wrapper = styled.div`
+  padding: 18px 30px;
+
+  p {
+    margin-top: 1rem;
+  }
+`
+
+const headers = [
+  {
+    name: 'Name',
+    value: 'name'
+  },
+  {
+    name: 'Description',
+    value: 'description'
+  },
+  {
+    name: 'Url',
+    value: 'html_url'
+  }
+]
 
 const HomePage = () => {
-  return (
-    <div>
-      <h1>{pluginId}&apos;s HomePage</h1>
-      <p>Happy coding</p>
-    </div>
-  );
-};
+  const [rows, setRows] = useState([])
 
-export default memo(HomePage);
+  useEffect(() => {
+    axios
+      .get('https://api.github.com/users/React-avancado/repos')
+      .then((res) => setRows(res.data))
+      .catch((e) =>
+        strapi.notification.error(`Ops... github API limit exceeded, ${e}`)
+      )
+  }, [])
+  return (
+    <Wrapper>
+      <Header
+        title={{ label: 'React Avançado Repositories' }}
+        content='A list of our repositories in React Avançado course.'
+      />
+      <Table headers={headers} rows={rows} />
+    </Wrapper>
+  )
+}
+
+export default memo(HomePage)
