c08e24e5a880e156fbcefc7934f57ed62b02c858
Eduardo Lima <dlima78@hotmail.com>
2021-06-01 17:02:40 -0300

    feat: populate the fields

--- api/game/controllers/game.js
+++ api/game/controllers/game.js
@@ -1,8 +1,16 @@
-'use strict';
+'use strict'
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
+module.exports = {
+  populate: async (ctx) => {
+    console.log('Starting to populate...')
 
-module.exports = {};
+    const options = {
+      sort: 'popularity',
+      page: '1',
+      ...ctx.query
+    }
+
+    await strapi.services.game.populate(options)
+    ctx.send('Finished populating!')
+  }
+}
