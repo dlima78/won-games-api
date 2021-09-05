c6b8a329c3dc56edd32873eaae697aa0e2f49440
Eduardo Lima <dlima78@hotmail.com>
2021-09-05 10:11:46 -0300

    feat: fetching payment cart data

--- api/order/controllers/order.js
+++ api/order/controllers/order.js
@@ -5,4 +5,23 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  createPaymentIntent: async (ctx) => {
+    const { cart } = ctx.request.body
+    let games = []
+
+    await Promise.all(
+      cart?.map(async game => {
+        const validatedGame = await strapi.services.game.findOne({
+          id: game.id
+        })
+  
+        if (validatedGame) {
+          games.push(validatedGame)
+        }
+      })
+    )
+
+    return games
+  }
+};
