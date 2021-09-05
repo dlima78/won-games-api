e662dba250b4a7a5f490903e8ed82786bef59e60
Eduardo Lima <dlima78@hotmail.com>
2021-09-05 10:30:23 -0300

    feat: calculating total games

--- api/order/controllers/order.js
+++ api/order/controllers/order.js
@@ -7,28 +7,32 @@
 
 module.exports = {
   createPaymentIntent: async (ctx) => {
-    const { cart } = ctx.request.body
-    let games = []
+    const { cart } = ctx.request.body;
+    let games = [];
 
     await Promise.all(
       cart?.map(async game => {
         const validatedGame = await strapi.services.game.findOne({
           id: game.id
-        })
+        });
   
         if (validatedGame) {
-          games.push(validatedGame)
+          games.push(validatedGame);
         }
       })
-    )
+    );
 
     if (!games.length) {
-      ctx.response.status  = 404
+      ctx.response.status  = 404;
       return {
         error: "No valid games found!"
-      }
+      };
     }
 
-    return games
+    const total = games.reduce((acc, game) => {
+      return acc + game.price;
+    }, 0)
+
+    return { total_in_cents: total * 100, games };
   }
 };
